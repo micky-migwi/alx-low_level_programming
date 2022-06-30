6c8d6bfdc7d1f016d4cdfb917489590bf1f667ed
micky-migwi <mickeys.dev26@gmail.com>
2022-06-30 11:35:27 -0700

    Adding 4-puts.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+   /**
+    * main - Should print: with proper grammar, but the outcome is a piece of art,
+    * Return: The program returns 0 when it ends
+    */
+int main(void)
+{ 
+  printf("with proper grammar, but the outcome is a piece of art,\n"); 
+  return (0);  
+}
