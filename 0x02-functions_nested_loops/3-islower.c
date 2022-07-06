9cd6a06c7ea64adcffb223a0b45c6f51e8fa7f13
micky-migwi <mickeys.dev26@gmail.com>
2022-07-06 07:26:38 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -10,14 +10,12 @@ int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		if (c >= 97 && c <= 122)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
-		_putchar('\n');
+		return (1);
 	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
 
